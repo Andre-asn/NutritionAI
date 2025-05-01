5820a88d9dcb7e2f9802987f38d9b2ba0edb0057
Andre Santiago-Neyra <125695806+Andre-asn@users.noreply.github.com>
2025-05-01 03:26:48 -0400

    Attmpe #3

--- nutritionai/src/App.jsx
+++ nutritionai/src/App.jsx
@@ -1,7 +1,8 @@
 import "./App.css";
 import React from "react";
-import { SignedOut, SignedIn, SignIn, SignUp, UserButton } from "@clerk/clerk-react";
-import { Routes, Route } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 import Upload from "./components/Upload.jsx";
 import Home from "./components/Home.jsx";
 import Profile from "./components/Profile.jsx";
@@ -9,28 +10,34 @@ import SignedOutPage from "./components/SignedOutPage.jsx";
 import SearchFriends from "./components/SearchFriends.jsx";
 import FriendsMeals from "./components/FriendsMeals.jsx";
 import Tracking from "./components/Tracking.jsx";
+import { SignIn, SignUp } from "@clerk/clerk-react";
 
 function App() {
-  return (
-    <div>
-      <SignedOut>
-        <SignedOutPage />
-      </SignedOut>
+  const { isSignedIn } = useUser();
 
-      <SignedIn>
-        {/* <UserButton /> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in/*" element={<SignIn />} />
-          <Route path="/sign-up/*" element={<SignUp />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/add-friends" element={<SearchFriends />} />
-          <Route path="/friends-meals" element={<FriendsMeals />} />
-          <Route path="/tracking" element={<Tracking />} />
-        </Routes>
-      </SignedIn>
-    </div>
+  return (
+    <Routes>
+      <Route path="/" element={isSignedIn ? <Home /> : <SignedOutPage />} />
+      <Route path="/sign-in/*" element={<SignIn />} />
+      <Route path="/sign-up/*" element={<SignUp />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route
+        path="/profile"
+        element={isSignedIn ? <Profile /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/add-friends"
+        element={isSignedIn ? <SearchFriends /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/friends-meals"
+        element={isSignedIn ? <FriendsMeals /> : <Navigate to="/" />}
+      />
+      <Route
+        path="/tracking"
+        element={isSignedIn ? <Tracking /> : <Navigate to="/" />}
+      />
+    </Routes>
   );
 }
 
